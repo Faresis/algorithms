53725953e4c7a394f3311198b654239fbe5a0573
Faresis <nikignatiev@gmail.com>
2017-12-19 11:14:46 +0200

    Simple sorting. Duplicates removal during sorting. Implementation.

--- src/ua/dp/mign/sorting/simple/inlineNoDuplicates.java
+++ src/ua/dp/mign/sorting/simple/inlineNoDuplicates.java
@@ -30,17 +30,33 @@ class ArrayInlineNoDuplicates {
     public void insertionSort() {
         int in, out;
 
+        final int DUP_KEY = -1;
         for (out = 1; out < nElems; out++)     // out is dividing line
         {
             long temp = a[out];            // remove marked item
             in = out;                      // start shifts at out
-            while (in > 0 && a[in - 1] >= temp) // until one is smaller,
+            while (in > 0 && a[in - 1] >= 0 && a[in - 1] >= temp) // until one is smaller,
             {
+               if (a[in - 1] == temp) {
+                   temp = DUP_KEY;
+               }
                 a[in] = a[in - 1];            // shift item to right
                 --in;                       // go left one position
             }
             a[in] = temp;                  // insert marked item
         }  // end for
+
+        int shift = 0;
+        for (int i = 0; i < nElems; i++) {
+            if (a[i] == DUP_KEY) {
+                shift--;
+                continue;
+            }
+            if (shift < 0) {
+                a[i + shift] = a[i];
+            }
+        }
+        nElems += shift;
     }  // end insertionSort()
 //--------------------------------------------------------------
 }  // end class ArrayIns
@@ -53,12 +69,17 @@ class InlineNoDuplicatesApp {
         arr = new ArrayInlineNoDuplicates(maxSize);  // create the array
 
         arr.insert(77);               // insert 10 items
+        arr.insert(77);               // insert 10 items
+        arr.insert(77);               // insert 10 items
+        arr.insert(77);               // insert 10 items
         arr.insert(99);
+        arr.insert(66);
         arr.insert(44);
         arr.insert(55);
         arr.insert(22);
         arr.insert(88);
         arr.insert(11);
+        arr.insert(99);
         arr.insert(00);
         arr.insert(66);
         arr.insert(33);
