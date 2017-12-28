12cb3bc049bb97e2a85f7f9931ecd164a0edb6f4
Faresis <nikignatiev@gmail.com>
2017-12-28 16:02:25 +0200

    Stack based on dequeue. Initial commit.

---
+++ src/ua/dp/mign/queues/StackOnDequeue.java
@@ -0,0 +1,67 @@
+package ua.dp.mign.queues;
+
+////////////////////////////////////////////////////////////////
+class StackOnDequeue {
+    private int maxSize;        // size of stack array
+    private long[] stackArray;
+    private int top;            // top of stack
+
+    //--------------------------------------------------------------
+    public StackOnDequeue(int s)         // constructor
+    {
+        maxSize = s;             // set array size
+        stackArray = new long[maxSize];  // create array
+        top = -1;                // no items yet
+    }
+
+    //--------------------------------------------------------------
+    public void push(long j)    // put item on top of stack
+    {
+        stackArray[++top] = j;     // increment top, insert item
+    }
+
+    //--------------------------------------------------------------
+    public long pop()           // take item from top of stack
+    {
+        return stackArray[top--];  // access item, decrement top
+    }
+
+    //--------------------------------------------------------------
+    public long peek()          // peek at top of stack
+    {
+        return stackArray[top];
+    }
+
+    //--------------------------------------------------------------
+    public boolean isEmpty()    // true if stack is empty
+    {
+        return (top == -1);
+    }
+
+    //--------------------------------------------------------------
+    public boolean isFull()     // true if stack is full
+    {
+        return (top == maxSize - 1);
+    }
+//--------------------------------------------------------------
+}  // end class StackOnDequeue
+
+////////////////////////////////////////////////////////////////
+class StackOnDequeueApp {
+    public static void main(String[] args) {
+        StackOnDequeue theStack = new StackOnDequeue(10);  // make new stack
+        theStack.push(20);               // push items onto stack
+        theStack.push(40);
+        theStack.push(60);
+        theStack.push(80);
+
+        while (!theStack.isEmpty())     // until it's empty,
+        {                             // delete item from stack
+            long value = theStack.pop();
+            System.out.print(value);      // display it
+            System.out.print(" ");
+        }  // end while
+        System.out.println("");
+    }  // end main()
+}  // end class StackApp
+////////////////////////////////////////////////////////////////
