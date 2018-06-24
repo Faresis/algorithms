84e8342afef5e7f9f0af5c0a76d64fead46ac6ef
Faresis <nikignatiev@gmail.com>
2018-06-24 13:46:35 +0300

    Heap. Priority Queue on Heap. Initial commit.

---
+++ src/ua/dp/mign/trees/heaps/priorityQ.java
@@ -0,0 +1,242 @@
+package ua.dp.mign.trees.heaps;
+
+// priorityQ.java
+// demonstrates priority queue
+// to run this program: C>java PriorityQApp
+////////////////////////////////////////////////////////////////
+class InternalHeapNode {
+    private int iData;             // data item (key)
+
+    // -------------------------------------------------------------
+    public InternalHeapNode(int key)           // constructor
+    {
+        iData = key;
+    }
+
+    // -------------------------------------------------------------
+    public int getKey() {
+        return iData;
+    }
+
+    // -------------------------------------------------------------
+    public void setKey(int id) {
+        iData = id;
+    }
+// -------------------------------------------------------------
+}  // end class Node
+////////////////////////////////////////////////////////////////
+
+class InternalHeap {
+    private InternalHeapNode[] heapArray;
+    private int maxSize;           // size of array
+    private int currentSize;       // number of nodes in array
+
+    // -------------------------------------------------------------
+    public InternalHeap(int mx)            // constructor
+    {
+        maxSize = mx;
+        currentSize = 0;
+        heapArray = new InternalHeapNode[maxSize];  // create array
+    }
+
+    // -------------------------------------------------------------
+    public boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    // -------------------------------------------------------------
+    public boolean insert(int key) {
+        if (currentSize == maxSize)
+            return false;
+        InternalHeapNode newNode = new InternalHeapNode(key);
+        heapArray[currentSize] = newNode;
+        trickleUp(currentSize++);
+        return true;
+    }  // end insert()
+
+    // -------------------------------------------------------------
+    public void trickleUp(int index) {
+        int parent = (index - 1) / 2;
+        InternalHeapNode bottom = heapArray[index];
+
+        while (index > 0 &&
+                heapArray[parent].getKey() < bottom.getKey()) {
+            heapArray[index] = heapArray[parent];  // move it down
+            index = parent;
+            parent = (parent - 1) / 2;
+        }  // end while
+        heapArray[index] = bottom;
+    }  // end trickleUp()
+
+    // -------------------------------------------------------------
+    public InternalHeapNode remove()           // delete item with max key
+    {                           // (assumes non-empty list)
+        InternalHeapNode root = heapArray[0];
+        heapArray[0] = heapArray[--currentSize];
+        trickleDown(0);
+        return root;
+    }  // end remove()
+
+    // -------------------------------------------------------------
+    public void trickleDown(int index) {
+        int largerChild;
+        InternalHeapNode top = heapArray[index];       // save root
+        while (index < currentSize / 2)       // while node has at
+        {                               //    least one child,
+            int leftChild = 2 * index + 1;
+            int rightChild = leftChild + 1;
+            // find larger child
+            if (rightChild < currentSize &&  // (rightChild exists?)
+                    heapArray[leftChild].getKey() <
+                            heapArray[rightChild].getKey())
+                largerChild = rightChild;
+            else
+                largerChild = leftChild;
+            // top >= largerChild?
+            if (top.getKey() >= heapArray[largerChild].getKey())
+                break;
+            // shift child up
+            heapArray[index] = heapArray[largerChild];
+            index = largerChild;            // go down
+        }  // end while
+        heapArray[index] = top;            // root to index
+    }  // end trickleDown()
+
+    // -------------------------------------------------------------
+    public boolean change(int index, int newValue) {
+        if (index < 0 || index >= currentSize)
+            return false;
+        int oldValue = heapArray[index].getKey(); // remember old
+        heapArray[index].setKey(newValue);  // change to new
+
+        if (oldValue < newValue)             // if raised,
+            trickleUp(index);                // trickle it up
+        else                                // if lowered,
+            trickleDown(index);              // trickle it down
+        return true;
+    }  // end change()
+
+    // -------------------------------------------------------------
+    public void displayHeap() {
+        System.out.print("heapArray: ");    // array format
+        for (int m = 0; m < currentSize; m++)
+            if (heapArray[m] != null)
+                System.out.print(heapArray[m].getKey() + " ");
+            else
+                System.out.print("-- ");
+        System.out.println();
+        // heap format
+        int nBlanks = 32;
+        int itemsPerRow = 1;
+        int column = 0;
+        int j = 0;                          // current item
+        String dots = "...............................";
+        System.out.println(dots + dots);      // dotted top line
+
+        while (currentSize > 0)              // for each heap item
+        {
+            if (column == 0)                  // first item in row?
+                for (int k = 0; k < nBlanks; k++)  // preceding blanks
+                    System.out.print(' ');
+            // display item
+            System.out.print(heapArray[j].getKey());
+
+            if (++j == currentSize)           // done?
+                break;
+
+            if (++column == itemsPerRow)        // end of row?
+            {
+                nBlanks /= 2;                 // half the blanks
+                itemsPerRow *= 2;             // twice the items
+                column = 0;                   // start over on
+                System.out.println();         //    new row
+            } else                             // next item on row
+                for (int k = 0; k < nBlanks * 2 - 2; k++)
+                    System.out.print(' ');     // interim blanks
+        }  // end for
+        System.out.println("\n" + dots + dots); // dotted bottom line
+    }  // end displayHeap()
+// -------------------------------------------------------------
+}  // end class Heap
+
+////////////////////////////////////////////////////////////////
+class PriorityQ {
+    // array in sorted order, from max at 0 to min at size-1
+    private int maxSize;
+    private long[] queArray;
+    private int nItems;
+
+    //-------------------------------------------------------------
+    public PriorityQ(int s)          // constructor
+    {
+        maxSize = s;
+        queArray = new long[maxSize];
+        nItems = 0;
+    }
+
+    //-------------------------------------------------------------
+    public void insert(long item)    // insert item
+    {
+        int j;
+
+        if (nItems == 0)                         // if no items,
+            queArray[nItems++] = item;         // insert at 0
+        else                                // if items,
+        {
+            for (j = nItems - 1; j >= 0; j--)         // start at end,
+            {
+                if (item > queArray[j])      // if new item larger,
+                    queArray[j + 1] = queArray[j]; // shift upward
+                else                          // if smaller,
+                    break;                     // done shifting
+            }  // end for
+            queArray[j + 1] = item;            // insert it
+            nItems++;
+        }  // end else (nItems > 0)
+    }  // end insert()
+
+    //-------------------------------------------------------------
+    public long remove()             // remove minimum item
+    {
+        return queArray[--nItems];
+    }
+
+    //-------------------------------------------------------------
+    public long peekMin()            // peek at minimum item
+    {
+        return queArray[nItems - 1];
+    }
+
+    //-------------------------------------------------------------
+    public boolean isEmpty()         // true if queue is empty
+    {
+        return (nItems == 0);
+    }
+
+    //-------------------------------------------------------------
+    public boolean isFull()          // true if queue is full
+    {
+        return (nItems == maxSize);
+    }
+//-------------------------------------------------------------
+}  // end class PriorityQ
+
+////////////////////////////////////////////////////////////////
+class PriorityQApp {
+    public static void main(String[] args) {
+        PriorityQ thePQ = new PriorityQ(5);
+        thePQ.insert(30);
+        thePQ.insert(50);
+        thePQ.insert(10);
+        thePQ.insert(40);
+        thePQ.insert(20);
+
+        while (!thePQ.isEmpty()) {
+            long item = thePQ.remove();
+            System.out.print(item + " ");  // 10, 20, 30, 40, 50
+        }  // end while
+        System.out.println("");
+    }  // end main()
+//-------------------------------------------------------------
+}  // end class PriorityQApp
+////////////////////////////////////////////////////////////////
