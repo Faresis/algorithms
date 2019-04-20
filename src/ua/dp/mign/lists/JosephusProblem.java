2beea20af614990235503b52750e7a10228de208
Faresis <nikignatiev@gmail.com>
2019-04-19 22:10:01 -0700

    Josephus problem implementation revised.

--- src/ua/dp/mign/lists/JosephusProblem.java
+++ src/ua/dp/mign/lists/JosephusProblem.java
@@ -2,22 +2,44 @@ package ua.dp.mign.lists;
 
 public final class JosephusProblem {
     public static void main(String[] args) {
-        int numberOfPeople = 7;
-        int countingOff = 2;
-
         SinglyLinkedCircularList list = new SinglyLinkedCircularList();
-        list.add(1);
-        for (int i = numberOfPeople; i > 1; i--) {
-            list.add(i);
+
+        int numOfElements = 7;
+        long numToBeLast = 2;
+
+        fillList(list, numOfElements);
+
+        list.display();
+
+        int countBy = findCountByForLast(list, numToBeLast);
+ 
+        System.out.println("In order to have " + numToBeLast + " to be the last from " + numOfElements);
+        System.out.println("Count by " + countBy + " should be performed.");
+    }
+
+    private static int findCountByForLast(SinglyLinkedCircularList list, long numToBeLast) {
+      for (int numOfSteps = 1; numOfSteps <= 100; numOfSteps++) {
+        SinglyLinkedCircularList specimen = list.copy();
+
+        long last = -1;
+        while(!specimen.isEmpty()) {
+          for (int i = 0; i < numOfSteps; i++) {
+            specimen.step();
+          }
+          last = specimen.remove();
         }
 
-        while (!list.isEmpty()) {
-            for (int i = 0; i < countingOff; i++) {
-                list.step();
-            }
-            System.out.println(list.remove());
-            if (!list.isEmpty())
-                list.step();
-        }
+        System.out.println("For " + numOfSteps + " number of steps the last number is " + last);
+
+        if (numToBeLast == last) return numOfSteps;
+      }
+      throw new IllegalArgumentException("Coudn't find number of steps for " + numToBeLast);
+    }
+
+    private static void fillList(SinglyLinkedCircularList list, int numOfElements) {
+      for (int i = 1; i <= numOfElements; i++) {
+        list.add(i);
+        list.step();
+      }
     }
 }
